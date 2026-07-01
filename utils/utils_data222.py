2f048aef2bb18c452d6faeb4e1de8bd9d69e58be
qing_kong <78006669+yudongjian@users.noreply.github.com>
2026-07-01 21:48:41 +0800

    Update utils_data222.py

--- utils/utils_data222.py
+++ utils/utils_data222.py
@@ -53,6 +53,8 @@ def get_DataLoader(args):
                                     # transforms.RandomHorizontalFlip(),
                                     # transforms.CenterCrop((256,256)),
                                     # transforms.ColorJitter(hue=0.05),
+                                    transforms.RandomHorizontalFlip(p=0.5),
+                                    transforms.RandomRotation(degrees=10),
                                     transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                     # transforms.Normalize([0.5, 0.5, 0.5, 0.5], [0.5, 0.5, 0.5, 0.5])
